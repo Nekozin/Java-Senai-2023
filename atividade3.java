6e72d97488562a1e1181fbfbe0c688e4228c6ab7
Dimas <125684532+Nekozin@users.noreply.github.com>
2023-04-14 13:52:13 -0300

    Add files via upload

---
+++ atividade3.java
@@ -0,0 +1,22 @@
+public class atividade3 {
+
+
+    public static void main(String[] args) {
+        
+        String nome = "Dimas dos Santos Vianna";
+        String crush = "Natacha";
+        String endereco = "Rua Carambola, 84, José Bonifácio";
+        int idade = 16;
+        char estciv = 'N';
+        double altura = 1.84;
+        String tel = "+55 (11) 913476-7268" ;
+
+        System.out.println("Meu nome é: "+nome);
+        System.out.println("O nome de minha crush é: "+crush);
+        System.out.println("Moro em "+endereco);
+        System.out.println("Tenho "+idade+" anos de idade");
+        System.out.println("Meu estado civil: "+estciv);
+        System.out.println("Tenho "+altura+" metros de altura");
+        System.out.println("Meu contato: "+tel);
+    }
+}
